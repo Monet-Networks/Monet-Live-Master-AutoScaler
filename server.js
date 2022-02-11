78d75955c81ab5cc8c916495c03cc2aa80897040
mayamalkoti-ashmar <mayank.malkoti@ashmar.in>
2022-02-11 15:16:45 +0530

    fix: instance

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ new db();
 const instanceRegistrationHandle = async (req, res) => {
   const instance = await createOneInstance(req, res);
   if (instance['code'] && instance['code'] === 400) return;
-  console.log('Instance Register >>>>>>>>', instance);
-  delete instance['_id'];
+  console.log('Instance Register >>>>>>>>', instance.lean());
   if (req.query.publicIP && instance) engine.addInstanceIP(instance);
 };
 
