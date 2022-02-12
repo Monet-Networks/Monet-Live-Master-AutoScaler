dc10857089f819cbaf508c53fc0adec4682ee3ef
mayamalkoti-ashmar <mayank.malkoti@ashmar.in>
2022-02-12 17:09:13 +0530

    feat:  added feature for scale up and scale down feature.

--- models/instance.model.js
+++ models/instance.model.js
@@ -8,10 +8,11 @@ const instanceSchema = mongoose.Schema({
   occupied: { type: Boolean, required: true },
   type: { type: String, enum: ['auto', 'manual'], default: 'manual' },
   CPU: { type: Number, default: 0 },
-  Upload: { type: String, default: '0' },
-  Download: { type: String, default: '0' },
-  Calls: { type: Number, default: 0 },
-  Participants: { type: Number, default: 0 },
+  Upload: { type: Number, defualt: 0 },
+  Download: { type: Number, defualt: 0 },
+  Calls: { type: Number, defualt: 0 },
+  Participants: { type: Number, defualt: 0 },
+  healthCheck: { type: String, enum: ['healthy', 'unhealthy'], default: 'healthy' },
 });
 
 module.exports = mongoose.model('instances', instanceSchema, 'instances');
