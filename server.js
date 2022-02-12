6943e058d8c0d44beb08d80c6b8e28b914c9d92f
mayamalkoti-ashmar <mayank.malkoti@ashmar.in>
2022-02-12 17:40:08 +0530

    feat:  added feature for scale up and scale down feature.

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ new db();
 const instanceRegistrationHandle = async (req, res) => {
   const instance = await createOneInstance(req, res);
   if (instance === "NaN") return;
-  if (req.query.publicIP && instance) engine.addInstance(instance);
+  console.log('Adding new instance to engine : ', instance);
+  if (instance.publicIP && instance) engine.addInstance(instance);
 };
 
 admin.post('/auth/google', googleAuth);
