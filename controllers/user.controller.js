24adfd4e9ed192371bdabdf694b35bac9c172516
mayamalkoti-ashmar <mayank.malkoti@ashmar.in>
2022-04-22 11:05:02 +0530

    fix : refactored user controller with fixes.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,7 +16,7 @@ const addRemainingHours = async (user) => {
   if (user.plan && user.plan.groupUid) {
     const planGroup = await PlanGroups.findOne({ uid: user.plan.groupUid });
     if (planGroup) {
-      user = { ...JSON.parse(JSON.stringify(user)), remainingHours: Math.round(planGroup.leftHours) };
+      user = { ...JSON.parse(JSON.stringify(user)), remainingHours: planGroup.leftHours.toFixed(2) };
     }
   }
 };
