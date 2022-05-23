d279a20232c5cfc5a761b319ea02dceca5cccdfe
Aashish Kaushik <aashish@ashmar.in>
2022-05-23 18:11:10 +0530

    🐛  rplace u

--- routes/index.js
+++ routes/index.js
@@ -546,8 +546,7 @@ admin.post('/v2/getreportsList', function (req, res) {
 admin.post('/authentication', async (req, res) => {
   try {
     const token = req.body;
-
-    const users = await auth.authenticate(token, u);
+    const users = await auth.authenticate(token, true);
     const remainingHours = await RemainingHours.addRemainingHours(user);
     const userPlan = await plan.find({ planUid: users.plan.planUid });
 
