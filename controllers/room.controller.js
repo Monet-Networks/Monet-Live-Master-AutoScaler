3a037dca80b74edc10e4a28a8135d7365b2b6bf6
Aashish Kaushik <aashish@ashmar.in>
2022-06-03 16:24:46 +0530

    fix

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -294,7 +294,7 @@ exports.V2getAllRooms = async function (req, res) {
         code: 200,
         error: false,
         message: 'The room exists',
-        response: {rooms,...rawData},
+        response: rooms,
       });
     }
   } catch (error) {
