754bc2d7fa8fbf93d199e815b65a2813e4e6556d
Aashish Kaushik <aashish@ashmar.in>
2022-06-14 10:14:32 +0530

    remove console

--- utils/genPdf.js
+++ utils/genPdf.js
@@ -33,9 +33,7 @@ const genPdf = async (roomid, creator_ID, redis) => {
       overallEngagement: report?.report?.averageEngagement || null,
       students: studentData,
     };
-    Reports.findOneAndUpdate({ roomid }, { pdf, creator_ID }, { upsert: true, new: true }).then((data) =>
-      console.log('New report with pdf', data)
-    );
+    Reports.findOneAndUpdate({ roomid }, { pdf, creator_ID }, { upsert: true, new: true });
     return pdf;
   } catch (err) {
     console.log('Generate report PDF error', err);
