6d38c90128835c2991df5ce4679305abbe1a2a22
Deeshan Sharma <deeshan.sharma@ashmar.in>
2022-06-15 10:18:58 +0530

    🔧 conf: add debug console log

--- utils/genPdf.js
+++ utils/genPdf.js
@@ -33,7 +33,9 @@ const genPdf = async (roomid, creator_ID, redis) => {
       overallEngagement: report?.report?.averageEngagement || null,
       students: studentData,
     };
-    Reports.findOneAndUpdate({ roomid }, { pdf, creator_ID }, { upsert: true, new: true });
+    Reports.findOneAndUpdate({ roomid }, { pdf, creator_ID }, { upsert: true, new: true }).then((data) =>
+      console.log('New report document', data)
+    );
     return pdf;
   } catch (err) {
     console.log('Generate report PDF error', err);
