699ae54153d216df68cc68edd38caa20c8bdd198
Aashish kaushik <aashish@ashmar.in>
2022-06-16 14:08:00 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -273,7 +273,11 @@ admin.get('/my-meetings', async (req, res) => {
           new Date(new Date().setHours(0, 0, 0, 0).valueOf() - 29 * 24 * 60 * 60 * 1000),
           new Date(new Date().setHours(23, 59, 59, 999)),
         ];
-  const reportsData = Reports.find({ creator_ID });
+  const reportsData = Reports.find({
+    creator_ID,
+    'start.dateTime': { $gte: new Date(start) },
+    'end.dateTime': { $lte: new Date(end) },
+  });
   const userRoomsData = Rooms.find({
     creator_ID,
     'start.dateTime': { $gte: new Date(start) },
