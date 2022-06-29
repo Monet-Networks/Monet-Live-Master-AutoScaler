265678260172d455a02a5c6adfa9e21fba3a5505
Aashish kaushik <aashish@ashmar.in>
2022-06-29 17:58:03 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -579,7 +579,7 @@ admin.get('/userPlanDetails', async (req, res) => {
       message: 'user not found for the email',
     });
   }
-
+  expired = userD.plan.type;
   planobject = await plan.find({ planUid: userD.plan.planUid }).lean();
 
   res.json({
@@ -587,6 +587,7 @@ admin.get('/userPlanDetails', async (req, res) => {
     error: false,
     message: 'Plan details  Found',
     data: planobject,
+    expired,
   });
 });
 
