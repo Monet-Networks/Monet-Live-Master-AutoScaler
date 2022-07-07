8d19cd558834c8c471a6250895bb0d7d515ec4ff
Aashish kaushik <aashish@ashmar.in>
2022-07-07 10:25:46 +0530

    fix error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('module-alias/register');
-require('@modules/socket-io');
+// require('@modules/socket-io');
 require('dotenv').config();
 const apiRoutes = require('@routes');
 const express = require('express');
