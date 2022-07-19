d035fc9c0a770d54c25160c8a4648adfaf0dd38a
Aashish kaushik <aashish@ashmar.in>
2022-07-19 12:14:45 +0530

    add time stamp in rroms model

--- models/reports.model.js
+++ models/reports.model.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
 
-const reportSchema = mongoose.Schema({
-  roomid: {
-    type: String,
-    index: true,
-    required: true,
+const reportSchema = mongoose.Schema(
+  {
+    roomid: {
+      type: String,
+      index: true,
+      required: true,
+    },
+    creator_ID: {
+      type: String,
+      index: true,
+      required: true,
+    },
+    pieData: {
+      type: [],
+      default: [],
+    },
+    overallEngagement: {
+      type: [],
+      default: [],
+    },
+    pdf: {
+      type: {},
+      default: null,
+    },
+    report: {
+      type: {},
+      default: null,
+    },
   },
-  creator_ID: {
-    type: String,
-    index: true,
-    required: true,
-  },
-  pieData: {
-    type: [],
-    default: [],
-  },
-  overallEngagement: {
-    type: [],
-    default: [],
-  },
-  pdf: {
-    type: {},
-    default: null,
-  },
-  report: {
-    type: {},
-    default: null,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('reports', reportSchema);
